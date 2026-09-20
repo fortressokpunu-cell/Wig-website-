f01a2c685436b94cc0b1f9db81fb1c905404d824
fortressokpunu-cell <fortressokpunu@gmail.com>
2026-09-20 05:40:35 +0100

    Create wigs.js

---
+++ wigs.js
@@ -0,0 +1,5 @@
+const wigs = [
+  { name: "Sample Wig 1", price: "Price here", image: "" },
+  { name: "Sample Wig 2", price: "Price here", image: "" },
+  { name: "Sample Wig 3", price: "Price here", image: "" }
+];
