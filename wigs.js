58f5c48108905dda7698c59980fc78011d19c6c1
fortressokpunu-cell <fortressokpunu@gmail.com>
2026-09-20 08:06:23 +0100

    Update wigs.js

--- wigs.js
+++ wigs.js
@@ -1,3 +1,4 @@
 const wigs = [
-  { name: "Natural Colour Bone Straight 30 inches", price: "Ask on WhatsApp", image: "6a2a2506-ed9f-41fe-ab05-38d4e985be87.jpeg" }
+  { name: "Natural Colour Bone Straight 30 inches", price: "Ask on WhatsApp", image: "6a2a2506-ed9f-41fe-ab05-38d4e985be87.jpeg" },
+  { name: "Black Bob Wig with Bangs", price: "Ask on WhatsApp", image: "a4cc35c2-9e9f-48a2-97a7-93588d6ff988.jpeg" }
 ];
